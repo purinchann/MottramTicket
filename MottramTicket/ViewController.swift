b8fb93b59844dc93b6edc8aff8a2cc1815204745
中村　鷹祐 <naka777you@gmail.com>
2019-05-27 21:06:59 +0900

    Initial Commit

---
+++ MottramTicket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MottramTicket
+//
+//  Created by 中村　鷹祐 on 2019/05/27.
+//  Copyright © 2019 中村　鷹祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
