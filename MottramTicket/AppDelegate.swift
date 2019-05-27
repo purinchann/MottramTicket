7cf628289056db712c5a5a01a28a6d9e44b6e3ea
中村　鷹祐 <naka777you@gmail.com>
2019-05-27 23:24:31 +0900

    firebaseを設定

--- MottramTicket/AppDelegate.swift
+++ MottramTicket/AppDelegate.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Firebase
+
+enum AppError: Error {
+    case noError
+    case network
+    case notAuthorized
+    case generic
+    case validate([Error])
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
